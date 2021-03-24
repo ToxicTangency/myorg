3ae1be115d7b2fac3898c49df83a19ed526b1070
Lemon <cvdrayr@gmail.com>
2021-03-24 23:54:45 +0500

    1212

--- src/todo/todolist.js
+++ src/todo/todolist.js
@@ -5,6 +5,10 @@ export default function TodoList(){
         <ul>
             <li>1</li>
             <li>2</li>
+            <li>1</li>
+            <li>2</li>
+            <li>1</li>
+            <li>2</li>
         </ul>
     );
 }
